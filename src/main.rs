10bbabb39cfb88fe64c90c6f123191132e21e40c
Qyriad <qyriad@qyriad.me>
2024-05-14 01:11:11 -0600

    require passing -W/--allow-worktree to mutate checked out refs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::error::Error as StdError;
+use std::iter;
 use std::path::PathBuf;
 
 use bstr::{BString, ByteSlice};
@@ -42,6 +43,11 @@ struct GitPointCmd
 	/// create a new ref of <KIND> instead of updating an existing one
 	#[arg(short, long, action = ArgAction::Set, value_name = "KIND")]
 	pub new: Option<NewRefKind>,
+
+	/// Allow mutating checked out refs.
+	/// Note that this will *not* change any of the actual files in the worktree.
+	#[arg(long, short = 'W', action = ArgAction::SetTrue)]
+	pub allow_worktree: bool,
 }
 
 /// The ref we will mutate.
@@ -134,6 +140,50 @@ impl<'repo> TargetRev<'repo>
 	}
 }
 
+/// Will std::process:exit() if check condition matches.
+fn check_worktrees(repo: &Repository, victim_ref: &Reference)
+{
+	let worktrees = repo
+		.worktrees()
+		.tap_err(|e| warn!("ignoring error finding active worktrees: {}", e))
+		.unwrap_or_else(|_e| Vec::new());
+
+	let worktree_repos = worktrees
+		.into_iter()
+		.filter_map(|worktree| {
+			let id = worktree.id().to_owned();
+			worktree
+				.into_repo_with_possibly_inaccessible_worktree()
+				.tap_err(|e| warn!("ignoring error accessing worktree {}: {}", id, e))
+				.ok()
+		})
+		.chain(iter::once(repo.clone()));
+
+	for tree_repo in worktree_repos {
+		let dir = tree_repo.work_dir().expect("unreachable");
+		eprintln!("looking at worktree {}", dir.display());
+
+		let tree_head = tree_repo
+			.head_ref()
+			.tap_err(|e| warn!("ignoring error discovering worktree {} HEAD: {}", dir.display(), e));
+		let Ok(tree_head) = tree_head else {
+			continue;
+		};
+
+		if tree_head.as_ref().map(|r| &r.inner) == Some(&victim_ref.inner) {
+			eprintln!(
+				"\x1b[91merror:\x1b[0m refusing to update ref \x1b[34m{}\x1b[0m checked out at {}; \
+				pass --allow-worktree to override",
+				victim_ref.name().shorten(),
+				dir.display(),
+			);
+
+			std::process::exit(1);
+		}
+	}
+
+}
+
 fn main() -> Result<(), Box<dyn StdError>>
 {
 	env_logger::builder()
@@ -149,7 +199,7 @@ fn main() -> Result<(), Box<dyn StdError>>
 	let repo: Repository = gix::open(&cwd)
 		.tap_err(|e| error!("while opening git repo in {}: {}", cwd.display(), e))?;
 
-	let ref_to_update = match &args.new {
+	let victim_ref = match &args.new {
 		Some(kind) => {
 			todo!();
 		},
@@ -157,12 +207,18 @@ fn main() -> Result<(), Box<dyn StdError>>
 			.tap_err(|e| error!("while finding reference {}: {}", &args.from, e))?
 	};
 
-	let victim = VictimRef::from(BString::from(args.from), &ref_to_update)?;
+	let victim = VictimRef::from(BString::from(args.from), &victim_ref)?;
 	let target = TargetRev::from(&repo, BString::from(args.to))?;
+
+	if !args.allow_worktree {
+		// Check if the victim *ref* is checked out anywhere.
+		// This function will exit the process if so.
+		check_worktrees(&repo, &victim_ref);
+	}
 
 	let reflog_msg = format!(
 		"git-point: updating {} from {} to {}",
-		ref_to_update.name().as_bstr(),
+		victim_ref.name().as_bstr(),
 		victim.resolved_id,
 		target.resolved_id,
 	);
@@ -177,7 +233,7 @@ fn main() -> Result<(), Box<dyn StdError>>
 			expected: PreviousValue::MustExistAndMatch(Target::Peeled(victim.resolved_id.into())),
 			new: Target::Peeled(target.resolved_id.detach()),
 		},
-		name: ref_to_update.name().to_owned(),
+		name: victim_ref.name().to_owned(),
 		deref: false,
 	};
 
@@ -185,7 +241,7 @@ fn main() -> Result<(), Box<dyn StdError>>
 
 	eprintln!(
 		"Updated \x1b[34m{refname}\x1b[0m from \x1b[33m{previd}\x1b[0m ({prevmsg}) to \x1b[33m{newid}\x1b[0m ({newmsg})",
-		refname = ref_to_update.name().shorten(),
+		refname = victim_ref.name().shorten(),
 		previd = victim.resolved_id.shorten_or_id(),
 		prevmsg = victim.summary.as_bstr(),
 		newid = target.resolved_id.shorten_or_id(),
